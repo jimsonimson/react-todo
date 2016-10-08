b3428943405da8566303cf66313f51a921fcfd37
Mike Hawk <jonathanimson@gmail.com>
2016-10-07 19:30:11 -0700

    Refactor todolist, todo, and addtodo to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,8 +2,8 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+import TodoList from 'TodoList'
+import AddTodo from 'AddTodo';
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
@@ -62,7 +62,7 @@ var TodoApp = React.createClass({
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
               <TodoSearch onSearch={this.handleSearch}/>
-              <TodoList todos={filterTodos} onToggle={this.handleToggle}/>
+              <TodoList/>
               <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
           </div>
--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -4,29 +4,33 @@ var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
 
-var AddTodo = require('AddTodo');
+var {AddTodo} = require('AddTodo');
 
 describe('AddTodo', () => {
   it('should exist', () => {
     expect(AddTodo).toExist();
   })
   
-  it('should call onAddTodo prop with valid data', () => {
-    var todoText = 'Check mail'
+  it('should dispatch ADD_TODO when valid todo text', () => {
+    var todoText = 'Check mail';
+    var action = {
+      type: 'ADD_TODO',
+      text: todoText
+    }
     var spy = expect.createSpy(); //1. create a spy
-    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>); //2. Render addtodo component and inject spy into onAddTodo prop
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>); //2. Render addtodo component and inject spy into onAddTodo prop
     var $el = $(ReactDOM.findDOMNode(addTodo)); //3. use jQuery to take DOM element of addtodo
     
     addTodo.refs.todoText.value = todoText; //4. Set the ref ="todoText" to Check mail
     TestUtils.Simulate.submit($el.find('form')[0]); //5. Submit the Check mail input
     
-    expect(spy).toHaveBeenCalledWith(todoText);
+    expect(spy).toHaveBeenCalledWith(action);
   });
   
-  it('should not call onAddTodo prop with invalid input', () => {
+  it('should not dispatch ADD_TODO when invalid todo text', () => {
     var todoText = '';
     var spy = expect.createSpy(); //1. create a spy
-    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>); //2. Render addtodo component and inject spy into onAddTodo prop
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>); //2. Render addtodo component and inject spy into onAddTodo prop
     var $el = $(ReactDOM.findDOMNode(addTodo)); //3. use jQuery to take DOM element of addtodo
     
     addTodo.refs.todoText.value = todoText; //4. Set the ref ="todoText" to Check mail
