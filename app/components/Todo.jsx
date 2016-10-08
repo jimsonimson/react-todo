0b2241c202eca6bc8ad6ec8a015043eabb5ed27e
Mike Hawk <jonathanimson@gmail.com>
2016-10-07 21:13:46 -0700

    refactor todosearch to work with redux store

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,26 +1,26 @@
 var React = require('react');
 var {connect} = require('react-redux');
 var moment = require('moment');
-var actions = require('actions')
+var actions = require('actions');
 
 export var Todo = React.createClass({
   render: function () {
     var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
-    var todoClassName = completed ? 'todo todo-completed' : 'todo'
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
-      var message = 'Created';
+      var message = 'Created ';
       var timestamp = createdAt;
-      
-      if(completed){
+
+      if (completed) {
         message = 'Completed ';
         timestamp = completedAt;
       }
-      
+
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
     };
+
     return (
-      <div className={todoClassName} onClick={()=>{
-          // this.props.onToggle(id);
+      <div className={todoClassName} onClick={() => {
           dispatch(actions.toggleTodo(id));
         }}>
         <div>
